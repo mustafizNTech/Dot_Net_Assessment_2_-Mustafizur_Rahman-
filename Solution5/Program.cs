2aab995e42d147228ba5305bb52b642a0249ed21
mustafizNTech <mustafizur.rahman@nashtechglobal.com>
2024-03-05 23:04:08 +0530

    added solution 5

--- Solution5/Program.cs
+++ Solution5/Program.cs
@@ -1,9 +1,45 @@
 ﻿using System;
-class Solution2 {
-    static string displayPeopleWhoLikedPost(string[] people){
-        
-        }
-  static void Main() {
-    
+
+public interface Activity{
+  void Execute();
+}
+
+public class WorkflowEngine{
+  public void Run(List<Activity> workflow){
+    foreach(var activity in workflow){
+      activity.Execute();
+    }
+  }
+}
+
+public class Activity1: Activity{
+  public void Execute(){
+    Console.WriteLine("Activity 1 executed.");
+  }
+}
+
+public class Activity2: Activity{
+  public void Execute(){
+    Console.WriteLine("Activity 2 executed.");
+  }
+}
+
+public class Activity3: Activity{
+  public void Execute(){
+    Console.WriteLine("Activity 3 executed.");
+  }
+}
+
+class Program{
+  static void Main(string[] args){
+    var workflow = new List<Activity>
+    {
+      new Activity1(),
+      new Activity2(),
+      new Activity3()
+    };
+
+    WorkflowEngine workflowEngine = new WorkflowEngine();
+    workflowEngine.Run(workflow);
   }
 }
