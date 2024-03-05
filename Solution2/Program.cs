d8c1d5f09d70b80d728f33bd27029644030065c6
mustafizNTech <mustafizur.rahman@nashtechglobal.com>
2024-03-05 14:28:10 +0530

    added solution 2

--- Solution2/Program.cs
+++ Solution2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Solution2 {
+    static string displayPeopleWhoLikesPost(string[] people){
+        if(people.Length == 0){
+            return "";
+        }
+        else if(people.Length == 1){
+            return people[0]+" likes your post.";
+        }
+        else if(people.Length == 2){
+            return people[0]+ " and " + people[1] + " like your post.";
+        }
+        else{
+            return (people[0]+", "+people[1] +" and "+ (people.Length-2) + " others like your post.");
+        }
+        
+    }
+  static void Main() {
+    string[] instance1 = {"Alpha", "Beta", "Gamma", "Delta"};
+    Console.WriteLine(displayPeopleWhoLikesPost(instance1));
+
+    string[] instance2 = {"Alpha", "Beta"};
+    Console.WriteLine(displayPeopleWhoLikesPost(instance2));
+
+    string[] instance3 = {"Alpha"};
+    Console.WriteLine(displayPeopleWhoLikesPost(instance3));
+
+    string[] instance4 = {};
+    Console.WriteLine(displayPeopleWhoLikesPost(instance4));
+
+    
+    
+  }
+}
