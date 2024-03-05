fb7eec62eb8212d104f39fdf4cc7f77d76bdc445
mustafizNTech <mustafizur.rahman@nashtechglobal.com>
2024-03-05 21:14:31 +0530

    added solution 4

--- Solution4/Program.cs
+++ Solution4/Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
-class Solution2 {
-    static string displayPeopleWhoLikedPost(string[] people){
-        
-        }
-  static void Main() {
-    
+class Post {
+  public string Title;
+  public string Description;
+  public DateTime CreationDate;
+  private int voteValue;
+
+  public Post(string title, string description){
+    Title = title;
+    Description = description;
+    CreationDate = DateTime.Now;
+    voteValue = 0;
+  }
+
+  public void upVote(){
+    voteValue++;
+  }
+
+  public void downVote(){
+    voteValue--;
+  }
+
+  public int getTotalVotes(){
+    return voteValue;
+  }
+}
+
+class Program{
+  static void Main(string[] args){
+    Post post = new Post("Post1", "This is description for Post1.");
+
+    post.upVote();
+    post.upVote();
+    post.downVote();
+    post.upVote();
+
+    Console.WriteLine("Total vote: " + post.getTotalVotes());
   }
 }
