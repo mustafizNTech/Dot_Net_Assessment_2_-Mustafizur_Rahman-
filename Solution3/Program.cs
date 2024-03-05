d0a7f3fabb9ad06561191cc1e9984dd253b42d24
mustafizNTech <mustafizur.rahman@nashtechglobal.com>
2024-03-05 15:13:52 +0530

    added solution 3

--- Solution3/Program.cs
+++ Solution3/Program.cs
@@ -1,9 +1,46 @@
 ﻿using System;
-class Solution2 {
-    static string displayPeopleWhoLikedPost(string[] people){
-        
+class Stopwatch {
+    private DateTime startingTime;
+    private TimeSpan timeElapsed;
+    private bool isStopwatchRunning;
+
+
+    public void Start(){
+        if(isStopwatchRunning){
+            throw new InvalidOperationException("Already stop watch already started!");
         }
-  static void Main() {
-    
-  }
+
+        startingTime = DateTime.Now;
+        isStopwatchRunning = true;
+    }
+
+    public void Stop(){
+        TimeSpan elapsed = DateTime.Now - startingTime;
+        timeElapsed+=elapsed;
+        isStopwatchRunning = false;
+    }
+
+    public TimeSpan GetElapsedTime(){
+        return timeElapsed;
+    }
+}
+
+class Program{
+    static void Main(string[] args){
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+        System.Threading.Thread.Sleep(2000);
+        stopwatch.Stop();
+
+        Console.WriteLine("Elapsed time: " + stopwatch.GetElapsedTime());
+
+        stopwatch.Start();
+
+        System.Threading.Thread.Sleep(1500);
+
+        stopwatch.Stop();
+
+        Console.WriteLine("Elapsed time: " + stopwatch.GetElapsedTime());
+    }
 }
